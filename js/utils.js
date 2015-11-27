e5974e1154c86d7b55586563a9ecd67b0baedb77
Interfacteur <gaetan@interfacteur.net>
2015-11-27 13:22:51 +0100

    utils
    prod and dev, evolutions

--- js/utils.js
+++ js/utils.js
@@ -6,8 +6,7 @@ novembre 2015 */
 
 var utilities = {
 
-	newnav: false,
-
+//analyze url
 	getPath: function () {
 		"use strict";
 		var pathname = decodeURIComponent(location.pathname),
@@ -30,9 +29,10 @@ var utilities = {
 			repo_owner: _owner,
 			repo_target: _target,
 			pathfull: _path,
-			//added later:
-			uri_base: document.location.protocol + "//" + document.location.host + _root + "/"
-/* EXEMPLES:
+			uri_base: document.location.protocol + "//" + document.location.host + _root + "/" //(added later)
+/*
+	EXEMPLES:
+
 http://www.domain.tld/githubapi/
 	root:			/githubapi
 	visu:			/
@@ -73,40 +73,42 @@ http://www.domain.tld/githubapi/form:malsup/malsup/form
 	repo_target:	malsup/form
 	pathfull:		/form:malsup/malsup/form
 
-http://www.domain.tld/githubapi/form/malsup/form/enoutre
-	root:			/githubapi
-	visu:			/form/malsup/form
-	repo_query:		form
-	repo_owner:		""
-	repo_target:	malsup/form
-	pathfull:		/form/malsup/form/enoutre
+				http://www.domain.tld/githubapi/form/malsup/form/enoutre
+					root:			/githubapi
+					visu:			/form/malsup/form
+					repo_query:		form
+					repo_owner:		""
+					repo_target:	malsup/form
+					pathfull:		/form/malsup/form/enoutre
 
-http://www.domain.tld/githubapi/form:malsup/malsup/form/enoutre
-	root:			/githubapi
-	visu:			/form:malsup/malsup/form
-	repo_query:		form
-	repo_owner:		:malsup
-	repo_target:	malsup/form
-	pathfull:		/form:malsup/malsup/form/enoutre
+				http://www.domain.tld/githubapi/form:malsup/malsup/form/enoutre
+					root:			/githubapi
+					visu:			/form:malsup/malsup/form
+					repo_query:		form
+					repo_owner:		:malsup
+					repo_target:	malsup/form
+					pathfull:		/form:malsup/malsup/form/enoutre
 
-http://www.domain.tld/githubapi/form/malsup/form/enoutre/encore
-	root:			/githubapi
-	visu:			/form/malsup/form
-	repo_query:		form
-	repo_owner:		""
-	repo_target:	malsup/form
-	pathfull:		/form/malsup/form/enoutre/encore
+				http://www.domain.tld/githubapi/form/malsup/form/enoutre/encore
+					root:			/githubapi
+					visu:			/form/malsup/form
+					repo_query:		form
+					repo_owner:		""
+					repo_target:	malsup/form
+					pathfull:		/form/malsup/form/enoutre/encore
 
-http://www.domain.tld/githubapi/form:malsup/malsup/form/enoutre/encore
-	root:			/githubapi
-	visu:			/form:malsup/malsup/form
-	repo_query:		form
-	repo_owner:		:malsup
-	repo_target:	malsup/form
-	pathfull:		/form:malsup/malsup/form/enoutre/encore
+				http://www.domain.tld/githubapi/form:malsup/malsup/form/enoutre/encore
+					root:			/githubapi
+					visu:			/form:malsup/malsup/form
+					repo_query:		form
+					repo_owner:		:malsup
+					repo_target:	malsup/form
+					pathfull:		/form:malsup/malsup/form/enoutre/encore
 */
 	};	},
 
+
+//fix numbers and dates
 	formatDecimal: function (n) {
 		"use strict";
 		return n < 10 ? "0" + n : n;
@@ -122,6 +124,9 @@ http://www.domain.tld/githubapi/form:malsup/malsup/form/enoutre/encore
 		return utilities.toGetDate(date) + " (" + date.getHours() + "h" + utilities.formatDecimal(date.getMinutes()) + ")";
 	},
 
+
+//capture keyboard events
+	newnav: false,
 	detectKeyboard: function () {
 		"use strict";
 		$(window).on("keydown", function (e) {
@@ -131,24 +136,85 @@ http://www.domain.tld/githubapi/form:malsup/malsup/form/enoutre/encore
 		}).on("keyup", function (e) {
 			"use strict";
 			utilities.newnav = false;
-});	}	};
+	});	},
 
 
-var styles = {
-	$b: $("body"),
-	toggle: function (c, val) {
-		"use strict";
-		this.$b.toggleClass(c, val);
-		return this;
-	},
-	loadingProgress: function (val) {
-		"use strict";
-		return this.toggle("loading", val);
-	},
-	hidingRepos: function (val) {
-		"use strict";
-		return this.toggle("hiding", val);
-}	};
+//toggle css classes
+	styles: {
+		$b: $("body"),
+		toggle: function (c, val) {
+			"use strict";
+			this.$b.toggleClass(c, val);
+			return this;
+		},
+		loadingProgress: function (val) {
+			"use strict";
+			return this.toggle("loading", val);
+		},
+		hidingRepos: function (val) {
+			"use strict";
+			return this.toggle("hiding", val);
+}	}	};
 
+
+
+//add the production scripts
+;(function () {
+	"use strict";
+
+	var $plug = $("#plug"),
+		compat = [],
+		compats = null,
+		nua = navigator.userAgent;
+
+	if ($plug.length == 0)
+		return;
+
+//emulation of API history
+	(	typeof history === "undefined"
+		||
+		typeof history.state === "undefined"
+	)
+	&& compat.push(
+		$("<script>", { "src" : plug.history }),
+		$("<script>", { "src" : plug.history_adapter })
+	);
+
+
+//emulation of promises
+	(	typeof Promise === "undefined"
+		||
+		Promise.toString().indexOf("[native code]") === -1
+	)
+	&& compat.push(
+		$("<script>", { "src" : plug.promises })
+	);
+
+
+//bug of clic on Safari 5
+	(	re.safari.test(nua)
+		&& Number(nua.split("Safari/")[1].split(".")[0]) < 535 //Safari 5
+		&& compat.push(
+			$("<script>", { "src" : plug.simulate })
+		)
+	)
+	|| ($.fn.simulate = function() {
+		this.get(0).click();
+	});
+
+
+	if (compat.length == 0)
+		return;
+
+//add the necessary scripts
+	compats = $("<div>");
+
+	$.each(compat, function (i) {
+		compats.append(compat[i]);
+	});
+
+	compats.insertAfter($plug);
+
+})();
 
 
